ed3e3e334668437aecd6677bd6543856319a3b87
LAURA SANTOS BANDEIRA <25132517@CFP127ALUNO.LOCAL>
2025-06-09 14:09:35 -0300

    Atualizando FileFactory

--- src/br/dev/laura/tarefas/factory/FileFactory.java
+++ src/br/dev/laura/tarefas/factory/FileFactory.java
@@ -1,7 +1,9 @@
 package br.dev.laura.tarefas.factory;
 
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class FileFactory {
@@ -11,12 +13,26 @@ public class FileFactory {
 	private FileWriter fw;
 	private BufferedWriter bw;
 
+	
+	private FileReader fr;
+	private BufferedReader br;
+	
+	
 	public FileFactory() throws IOException {
+		//importante para gravar no arquivo
 		fw = new FileWriter(arquivo, true);
 		bw = new BufferedWriter(fw);
+		
+		//importante para ler no arquivo
+		fr = new FileReader(arquivo);
+		br = new BufferedReader(fr);
 	}
 
 	public BufferedWriter getBufferedWriter() {
 		return bw;
+	}
+	
+	public BufferedReader getBufferedReader() {
+		return br;
 	}
 }
